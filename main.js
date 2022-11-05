e0601d7052842c139451f087be8326f54ea0f27c
btxfleurs <101543322+btxfleurs@users.noreply.github.com>
2022-11-05 01:37:29 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+window.addEventListener('scroll', onScroll)
+
+function onScroll() {
+  showNavOnScroll()
+}
+
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navegacao.classList.add('scroll')
+  } else {
+    navegacao.classList.remove('scroll')
+  }
+}
+
 function openMenu() {
   document.body.classList.add('menu-expanded')
 }
